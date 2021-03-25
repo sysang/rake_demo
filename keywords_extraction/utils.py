76dec6a21dd8ec7cc5ff7d78e57943a3711092c9
sysang <daosysang@gmail.com>
2021-03-25 13:52:46 +0700

    Checked.

--- keywords_extraction/utils.py
+++ keywords_extraction/utils.py
@@ -3,5 +3,6 @@ PREPOSITIONS_ARTICLES = [
         "langs", "met", "na", "naar", "naast", "om", "onder", "op", "over", "rond", "sinds",
         "te", "tegen", "tegenover", "got", "tussen", "uit", "van", "voor", "zonder",
         "de", "het", "en", "of", "hoe", "wat", "die", "dat", "dit", "wanneer", "waar", "maar", ",",
-        "mijn", "jouw", "je", "uw", "zijn", "haar", "ons", "onze", "hun"
+        "mijn", "jouw", "je", "uw", "zijn", "haar", "ons", "onze", "hun",
+        "hele"
         ]
